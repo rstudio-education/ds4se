9012f8063bdbadd8bfc3e458e0713e1690601a0a
YimRegister <yimregister@gmail.com>
2019-06-24 15:39:31 -0700

    adding run instructions for acorn example

--- bin/JSFiles/acorn_example.js
+++ bin/JSFiles/acorn_example.js
@@ -1,3 +1,18 @@
+/*
+This is a program that calls upon the acorn JavaScript parser to look at abstract syntax trees of JS programs.  I have used a bash script to call this program on a directory of JS programs, which writes the number of lines and number of functions to a Stats/js_stats.csv file. Currently, it touches the file twice, which is a shame. First, to count the number of functions, next to count the number of lines. I couldn't get them to read simultaneously.
+
+JavaScript Files: https://github.com/trekhleb/javascript-algorithms.git
+
+Bash script:
+Inside the directory with all your python files, run this. It will write to "Stats/js_stats.csv"
+
+ for f in Programs/*.js; do node acorn_example.js "$f" >> Stats/js_stats.csv; done
+
+ To clear the Stats/python_stats.csv file, run:
+ cat /dev/null > Stats/js_stats.csv
+
+*/
+//console.log(process.argv[2]);
 var acorn = require("acorn");
 var i;
 var line_count = 0;
@@ -5,6 +20,8 @@ var func_count=0;
 var test = function(){
   var x = 2;
 }
+
+//TODO: this just can't be the right way to print out how many functions in a file. There's too many zeros or in the order of magnitude of thousands.
 require('fs').createReadStream(process.argv[2])
   .on('data', function(chunk) {
     for (i=0; i < chunk.length; ++i){
@@ -17,7 +34,7 @@ require('fs').createReadStream(process.argv[2])
   }
   })
   .on('end', function() {
-    console.log(func_count);
+    //console.log(func_count);
   });
 require('fs').createReadStream(process.argv[2])
   .on('data', function(chunk) {
@@ -25,5 +42,5 @@ require('fs').createReadStream(process.argv[2])
       if (chunk[i] == 10) line_count++;
   })
   .on('end', function() {
-    console.log(line_count);
+    console.log(func_count + " , " +line_count);
   });
